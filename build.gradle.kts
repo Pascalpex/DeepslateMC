5957c5dd457386f84ed35ce9ab521600104b4970
Pascalpex <68245106+Pascalpex@users.noreply.github.com>
2021-12-21 13:40:04 +0100

    Shadow 7.1.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     java
     `maven-publish`
-    id("com.github.johnrengelman.shadow") version "7.1.0" apply false
+    id("com.github.johnrengelman.shadow") version "7.1.1" apply false
     id("io.papermc.paperweight.patcher") version "1.1.14"
 }
 
