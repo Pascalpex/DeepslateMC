4d88911aae3f02717865d698127dcfb398c74f5d
Pascalpex <pascalpex@gmail.com>
2022-07-14 15:37:24 +0200

    Minor updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ repositories {
 }
 
 dependencies {
-    remapper("net.fabricmc:tiny-remapper:0.8.4:fat")
+    remapper("net.fabricmc:tiny-remapper:0.8.5:fat")
     decompiler("net.minecraftforge:forgeflower:1.5.605.7")
     paperclip("io.papermc:paperclip:3.0.2")
 }
