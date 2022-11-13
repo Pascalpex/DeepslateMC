1fcf14fda5038afba3996fced30a78dc5377451c
Pascalpex <pascalpex@gmail.com>
2022-11-13 13:13:19 +0100

    Build cleanup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,16 @@
-import io.papermc.paperweight.util.constants.*
-
 plugins {
     java
     `maven-publish`
     id("com.github.johnrengelman.shadow") version "7.1.2" apply false
-    id("io.papermc.paperweight.patcher") version "1.3.8"
+    id("io.papermc.paperweight.patcher") version "1.3.9"
 }
+
+val paperMavenPublicUrl = "https://papermc.io/repo/repository/maven-public/"
 
 repositories {
     mavenCentral()
-    maven("https://repo.papermc.io/repository/maven-public/") {
-        content { onlyForConfigurations(PAPERCLIP_CONFIG) }
+    maven(paperMavenPublicUrl) {
+        content { onlyForConfigurations(configurations.paperclip.name) }
     }
 }
 
@@ -45,38 +45,32 @@ subprojects {
 
     repositories {
         mavenCentral()
+        maven(paperMavenPublicUrl)
         maven("https://oss.sonatype.org/content/groups/public/")
-        maven("https://repo.papermc.io/repository/maven-public/")
         maven("https://ci.emc.gs/nexus/content/groups/aikar/")
         maven("https://repo.aikar.co/content/groups/aikar")
         maven("https://repo.md-5.net/content/repositories/releases/")
         maven("https://hub.spigotmc.org/nexus/content/groups/public/")
         maven("https://jitpack.io")
-        maven("https://repo.codemc.org/repository/maven-public/")
     }
 }
 
 paperweight {
     serverProject.set(project(":deepslateMC-server"))
 
-    remapRepo.set("https://maven.fabricmc.net/")
-    decompileRepo.set("https://files.minecraftforge.net/maven/")
+    remapRepo.set(paperMavenPublicUrl)
+    decompileRepo.set(paperMavenPublicUrl)
 
     useStandardUpstream("purpur") {
         url.set(github("PurpurMC", "Purpur"))
         ref.set(providers.gradleProperty("purpurRef"))
 
         withStandardPatcher {
-            baseName("purpur")
-
             apiOutputDir.set(layout.projectDirectory.dir("deepslateMC-api"))
             serverOutputDir.set(layout.projectDirectory.dir("deepslateMC-server"))
 
             apiSourceDirPath.set("Purpur-API")
             serverSourceDirPath.set("Purpur-Server")
-
-            remapRepo.set("https://maven.fabricmc.net/")
-            decompileRepo.set("https://files.minecraftforge.net/maven/")
         }
 
     }
