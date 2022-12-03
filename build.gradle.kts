ab87a66b3cdda01e010315f0b41cfdd67da10dbd
Pascalpex <pascalpex@gmail.com>
2022-12-03 16:17:49 +0100

    Updated dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ repositories {
 
 dependencies {
     remapper("net.fabricmc:tiny-remapper:0.8.6:fat")
-    decompiler("net.minecraftforge:forgeflower:2.0.605.1")
+    decompiler("org.quiltmc:quiltflower:1.9.0")
     paperclip("io.papermc:paperclip:3.0.2")
 }
 
