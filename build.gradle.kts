665ea2b2ebd1a6b2abcd08f56db36bc349e17115
Pascalpex <pascalpex@gmail.com>
2024-03-15 15:33:52 +0100

    Updated Upstream (Leaf)

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     java
     `maven-publish`
     id("com.github.johnrengelman.shadow") version "8.1.1" apply false
+    id("io.github.goooler.shadow") version "8.1.7" apply false
     id("io.papermc.paperweight.patcher") version "1.5.11"
 }
 
