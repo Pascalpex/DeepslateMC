08d59cad4fe62e1ea3db1a0c9f0fb291a9442521
Pascalpex <pascalpex@gmail.com>
2024-06-21 19:37:09 +0200

    Use real Leaf

--- build.gradle.kts
+++ build.gradle.kts
@@ -61,7 +61,7 @@ paperweight {
     decompileRepo.set(paperMavenPublicUrl)
 
     useStandardUpstream("leaf") {
-        url.set(github("Pascalpex", "Leaf"))
+        url.set(github("Winds-Studio", "Leaf"))
         ref.set(providers.gradleProperty("leafRef"))
 
         withStandardPatcher {
