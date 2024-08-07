8bb112850d3379ad7afaf741d0fb52b40962b34d
Pascalpex <pascalpex@gmail.com>
2024-08-07 16:43:46 +0200

    Paperweight 1.7.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     java
     `maven-publish`
-    id("io.papermc.paperweight.patcher") version "1.7.1"
+    id("io.papermc.paperweight.patcher") version "1.7.2"
 }
 
 val paperMavenPublicUrl = "https://papermc.io/repo/repository/maven-public/"
