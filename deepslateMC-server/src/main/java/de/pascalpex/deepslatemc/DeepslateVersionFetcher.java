9fe41f22861dc6e67de337afe87a4d52f729ad80
Pascalpex <pascalpex@gmail.com>
2025-03-28 20:34:39 +0100

    Begin 1.21.5 update

--- deepslateMC-server/src/main/java/de/pascalpex/deepslatemc/DeepslateVersionFetcher.java
+++ deepslateMC-server/src/main/java/de/pascalpex/deepslatemc/DeepslateVersionFetcher.java
@@ -5,7 +5,7 @@ import org.galemc.gale.version.AbstractPaperVersionFetcher;
 public class DeepslateVersionFetcher extends AbstractPaperVersionFetcher {
     public DeepslateVersionFetcher() {
         super(
-            "ver/1.21.4",
+            "ver/1.21.5",
             "https://pascalpex.de/deepslate/",
             "Pascalpex",
             "DeepslateMC",
