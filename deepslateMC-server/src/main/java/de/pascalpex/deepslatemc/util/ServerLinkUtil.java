26eb2606b8733cdc7728ffa40e35abec0deb41ba
Pascalpex <pascalpex@gmail.com>
2025-06-15 21:30:21 +0200

    Add configurable server links

---
+++ deepslateMC-server/src/main/java/de/pascalpex/deepslatemc/util/ServerLinkUtil.java
@@ -0,0 +1,41 @@
+package de.pascalpex.deepslatemc.util;
+
+import de.pascalpex.deepslatemc.files.Config;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import org.apache.logging.log4j.LogManager;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ServerLinkUtil {
+
+    private static Map<Component, URI> links = null;
+
+    public static void loadLinks() {
+        if (links == null) {
+            links = new HashMap<>();
+        } else {
+            links.clear();
+        }
+        Config.getServerLinks().forEach((key, value) -> {
+            if(key.isBlank() || value.isBlank()) {
+                return;
+            }
+            try {
+                links.put(MiniMessage.miniMessage().deserialize(key), new URI(value));
+            } catch (URISyntaxException e) {
+                LogManager.getLogger(ServerLinkUtil.class.getSimpleName()).warn("Invalid URL in DeepslateMC server links: {}", value);
+            }
+        });
+    }
+
+    public static Map<Component, URI> getLinks() {
+        if (links == null) {
+            loadLinks();
+        }
+        return links;
+    }
+}
